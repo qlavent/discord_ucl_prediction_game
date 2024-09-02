bd8203f9bf2096484fb8d5fa8f90876e628af3b5
qlavent <quinten.lavent@ugent.be>
2024-09-02 13:51:07 +0200

    firestore database created

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import json
 from datetime import datetime, timedelta
 from data_fetch import fetch_prem_today
 from keep_alive import keep_alive
-
-keep_alive()
+import firestore_interaction
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -22,13 +21,18 @@ class MyClient(discord.Client):
     # don't respond to ourselves
     if message.author == self.user:
       return
-
-    if message.content.lower() == 'ping':
+    
+    msg = message.content.lower()
+    if msg == 'ping':
       await message.channel.send('pong')
 
-    if message.content.lower() == 'prem':
+    if msg == 'prem':
       prem_games = fetch_prem_today()
       await message.channel.send(prem_games)
+
+    if msg ==  'store':
+      firestore_interaction.store_data()
+      await message.channel.send('data is stored normally')
 
 
 intents = discord.Intents.default()
