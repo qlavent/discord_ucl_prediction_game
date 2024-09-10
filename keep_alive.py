a9c98216ae84be67bac59f4edda19b8f3138e9db
qlavent <quinten.lavent@ugent.be>
2024-09-10 13:36:44 +0200

    pre deploy, update keep alive

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,19 @@
+# keep_alive.py
+
 from flask import Flask
 from threading import Thread
+import os
 
-app = Flask(__name__)
+app = Flask('')
 
 @app.route('/')
 def home():
     return "I'm alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Bind to the port that Render assigns or use port 8080 as default
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
